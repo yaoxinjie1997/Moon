34a45d6768de46c6a28e12196398ade502536ad6
yaoxinjie <1072102644@qq.com>
2018-05-28 16:46:52 +0800

    列表页所有小功能完善只差传递数据

--- js/liebiao.js
+++ js/liebiao.js
@@ -13,42 +13,56 @@ $(function(){
 	}
 	lb_hddb();
 	//顶部悬浮的js
-	var $t_xf= $("#top .header_wrap .header_bottom");
-	var $n_xf=$("nav");
-	$(window).scroll(function(){
-		if($(this).scrollTop()>=50){
-			$t_xf.css({
-				"position":"fixed",
-				"background":"#333",
-				"z-index":"3",
-				"top":"0",
-				"width":"100%",
-				"left":"0",
-				"opacity":".9"
-			})
-		}else{
-			$t_xf.css({
-				"position":"static",
-				"opacity":"1"
-			})
-		}
-		if($(this).scrollTop()>=92){
-			$n_xf.css({
-				"position":"fixed",
-				"z-index":"3",
-				"top":"42px",
-				"width":"100%",
-				"left":"0",
-				"opacity":".9",
-				"border-bottom":"1px solid #ddd"
-			})
-		}else{
-			$n_xf.css({
-				"position":"static",
-				"opacity":"1",
-				"border-bottom":"none"
-			})
-		}
-	})
-	
+	function lb_xf(){
+		var $t_xf= $("#top .header_wrap .header_bottom");
+		var $n_xf=$("nav");
+		$(window).scroll(function(){
+			if($(this).scrollTop()>=50){
+				$t_xf.css({
+					"position":"fixed",
+					"background":"#333",
+					"z-index":"3",
+					"top":"0",
+					"width":"100%",
+					"left":"0",
+					"opacity":".9"
+				})
+			}else{
+				$t_xf.css({
+					"position":"static",
+					"opacity":"1"
+				})
+			}
+			if($(this).scrollTop()>=92){
+				$n_xf.css({
+					"position":"fixed",
+					"z-index":"3",
+					"top":"42px",
+					"width":"100%",
+					"left":"0",
+					"opacity":".9",
+					"border-bottom":"1px solid #ddd"
+				})
+			}else{
+				$n_xf.css({
+					"position":"static",
+					"opacity":"1",
+					"border-bottom":"none"
+				})
+			}
+		})
+	}
+	lb_xf();
+	//物品滑过尺码滑出
+	function lb_size(){
+		var $list= $(".lb-wp-c ul li");
+		$list.mouseenter(function(){
+			$(this).find(".wp-size").slideDown("fast")
+		})
+		$list.mouseleave(function(){
+			$(this).find(".wp-size").slideUp("fast")
+		})
+	}
+	lb_size();
+	//获取数据
 })
