1fff478215b34a54dbb9d438a667af4aa291f17d
yaoxinjie <1072102644@qq.com>
2018-05-28 17:36:32 +0800

    列表页传递数据完成  还有一些小瑕疵没有布局

--- js/liebiao.js
+++ js/liebiao.js
@@ -1,5 +1,52 @@
 
 $(function(){
+	//获取数据
+	$.get("http://h6.duchengjiu.top/shop/api_goods.php",{//101 112 125
+		"cat_id":45
+	},function(data){
+		//console.log(data);
+		//console.log(data.data);
+		var str="";
+		var $wplist=$(".lb-wp-c>ul");
+		//console.log($wplist);
+		$(data.data).each(function(){
+			//console.log($(this)[0])
+			str+=`<li>
+						<a href="#" cat_id="${$(this)[0].goods_id}"><img src="${$(this)[0].goods_thumb}"/></a>
+						<div class="wp-list">
+							<h6>${$(this)[0].goods_name}</h6>
+							<p><a href="#">${$(this)[0].goods_desc}</a></p>
+							<b>￥<span>${$(this)[0].price}</span></b>
+						</div>
+						<div class="wp-size">
+							<p>没有这个属性我也很无奈:</p>
+							<ul>
+								<li><span>尺码</span></li>
+								<li><span>尺码</span></li>
+								<li><span>尺码</span></li>
+							</ul>
+						</div>
+					</li>`
+		})
+		$wplist.html(str)
+		//物品滑过尺码滑出
+	function lb_size(){
+		var $list= $(".lb-wp-c>ul>li");
+		$list.each(function(){
+			if(($(this).index()+1)%4==0){
+				//console.log($(this).index())
+				$(this).css({"margin-right":"0"})
+			}
+		})
+		$list.mouseenter(function(){
+			$(this).find(".wp-size").slideDown("fast")
+		})
+		$list.mouseleave(function(){
+			$(this).find(".wp-size").slideUp("fast")
+		})
+	}
+	lb_size();
+	})	
 	//右侧导航回到顶部的js
 	function lb_hddb(){
 		var $hddb=$(".lb-r-dh").find("a").eq(3);
@@ -53,16 +100,5 @@ $(function(){
 		})
 	}
 	lb_xf();
-	//物品滑过尺码滑出
-	function lb_size(){
-		var $list= $(".lb-wp-c ul li");
-		$list.mouseenter(function(){
-			$(this).find(".wp-size").slideDown("fast")
-		})
-		$list.mouseleave(function(){
-			$(this).find(".wp-size").slideUp("fast")
-		})
-	}
-	lb_size();
-	//获取数据
+	
 })
