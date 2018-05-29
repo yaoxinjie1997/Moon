749146e5673b2508d131c7c96adf53dcca6f94f3
yaoxinjie <1072102644@qq.com>
2018-05-29 15:45:24 +0800

    放大镜功能完成

--- js/xiangqing.js
+++ js/xiangqing.js
@@ -3,25 +3,26 @@ $(function(){
 	$.get("http://h6.duchengjiu.top/shop/api_goods.php",{
 		"goods_id":249851
 	},function(data){
-		console.log(data.data[0])
+		//console.log(data.data[0])
 		var $data=data.data[0];
 		$(".wpxq-r .wpxq-r-t h5").html($data.goods_desc);//物品标题
 		$(".wpxq-r .wpxq-r-t p b span").html($data.goods_id);//物品编号
 		$(".wpxq-r .wpxq-r-c").find("p").eq(0).find("span").html($data.price);//物品价格
 		$(".wpxq-t .wpxq-l-t").find("img").eq(0).attr("src",$data.goods_thumb);//物品图片
+		$("#fangdajing").find("img").attr("src",$data.goods_thumb);//物品放大图片
 		$(".wpxq-t .wpxq-l-b ul li img").attr("src",$data.goods_thumb);//物品缩略图
 	})
 	//放大镜
 	var $wpxq_k=$("#wpxq .wpxq-t .wpxq-l .wpxq-l-t");
 	var $huakuai=$wpxq_k.find("#huadongkuai")
 	var $fangdajing=$("#wpxq .wpxq-t .wpxq-l").find("#fangdajing")
-	console.log($wpxq_k,$fangdajing)
+	//console.log($wpxq_k,$fangdajing)
 	$wpxq_k.mousemove(function(e){
 		var evt=e||event;
 		$huakuai.css("display","block")
 		$fangdajing.css("display","block")
-		var $x=evt.pageX;
-		var $y=evt.pagey;
+		var $x=evt.pageX-$huakuai.width()/2-132;
+		var $y=evt.pageY-$huakuai.height()/2-193;
 		if($x<=0){
 			$x=0;
 		}
@@ -38,8 +39,12 @@ $(function(){
 			"top":$y,
 			"left":$x
 		})
-		console.log($x)
-		
+		//console.log($huakuai.offset().top-$huakuai.height()-93,$wpxq_k.width(),$fangdajing.find("img").width())  //不知为何要left要减去32.5  top要减去93
+		$fangdajing.find("img").css({
+			"left":-($huakuai.offset().left-$huakuai.width()-32.5)/$wpxq_k.width()*$fangdajing.find("img").width(),
+			
+			"top":-($huakuai.offset().top-$huakuai.height()-93)/$wpxq_k.height()*$fangdajing.find("img").height()
+		})
 	})
 	$wpxq_k.mouseout(function(){
 		$huakuai.css("display","none")
